68c4fd6cf147dbf1b7ac75863d7dfc8c64662cb2
mahrokh <mahrokh.nz@gmail.com>
2022-02-02 22:48:33 +0330

    feat: add mousedown event

--- projects/paint/script.js
+++ projects/paint/script.js
@@ -2,17 +2,21 @@ const board = document.querySelector('.board')
 const ctx = board.getContext("2d");
 let circle = document.createElement("div");
 
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(300, 150);
-ctx.stroke();
+board.addEventListener('mousedown', (e) => {
+    const x = e.pageX || e.clientX
+    const y = e.pageY || e.clientY
+
+    
+})
 
 board.addEventListener('mousemove', (e) => {
     const x = e.pageX || e.clientX
     const y = e.pageY || e.clientY
 
-    document.body.appendChild(circle)
-    circle.classList.add('circlePointer')
-    circle.style.left = `${x}px`
-    circle.style.top = `${y}px`
+    // document.body.appendChild(circle)
+    // circle.classList.add('circlePointer')
+    // circle.style.left = `${x}px`
+    // circle.style.top = `${y}px`
 })
+
+
