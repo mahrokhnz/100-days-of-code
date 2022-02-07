ee3951fe5ea60ea138251fda717cf9ae1b4c1e65
mahrokh <mahrokh.nz@gmail.com>
2022-02-07 22:32:07 +0330

    feat: add done task event and change style

--- projects/to_do_list/script.js
+++ projects/to_do_list/script.js
@@ -3,14 +3,21 @@ const div = document.querySelector(".div");
 const main = document.querySelector(".main");
 const addButton = document.querySelector(".addButton");
 
+let toDoItem = null;
+let isWrite = false;
+
 function addToDoHandler() {
   //parent div
   const row = document.createElement("div");
   document.body.appendChild(row);
   main.appendChild(row);
   row.classList.add("toDoItems");
+  const text = document.createElement("span");
+  row.appendChild(text);
+  text.innerText = toDoItem;
 
   //childs
+  //remove
   const deleteIcon = document.createElement("div");
   document.body.appendChild(deleteIcon);
   row.appendChild(deleteIcon);
@@ -19,8 +26,30 @@ function addToDoHandler() {
   deleteIcon.addEventListener("click", () => {
     row.remove();
   });
+
+  //line-through
+  const crossToDo = document.createElement("div");
+  document.body.appendChild(crossToDo);
+  row.appendChild(crossToDo);
+  crossToDo.classList.add("crossToDo");
+
+  crossToDo.addEventListener("click", () => {
+    text.style.textDecoration = "line-through";
+  });
 }
 
 addButton.addEventListener("click", () => {
-  addToDoHandler();
+  if (isWrite) {
+    addToDoHandler();
+    toDo.value = "";
+  }
+});
+
+toDo.addEventListener("input", (e) => {
+  if (e.target.value.length > 0) {
+    toDoItem = e.target.value;
+    isWrite = true;
+  } else {
+    toDoItem = "";
+  }
 });
