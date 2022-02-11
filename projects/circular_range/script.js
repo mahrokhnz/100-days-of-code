4cd03b65f12d5e40b04c0faf1bc68b81a4799942
mahrokh <mahrokh.nz@gmail.com>
2022-02-11 16:25:32 +0330

    refactor: replace div with svg

--- projects/circular_range/script.js
+++ projects/circular_range/script.js
@@ -1,41 +1,73 @@
 const range = document.querySelector(".range");
-const pointer = document.querySelector(".pointer");
-const svg = document.querySelector(".svg");
+const circle = document.querySelector(".circle");
 
 let isDragging = false;
 let startMousePos = { x: 0, y: 0 };
 let startDraggablePos = { x: 0, y: 0 };
 
-pointer.addEventListener("mousedown", (e) => {
-  startMousePos.x = e.pageX || e.clientX;
-  startMousePos.y = e.pageY || e.clientY;
+// circle.addEventListener("mousedown", (e) => {
+//   startMousePos.x = e.pageX || e.clientX;
+//   startMousePos.y = e.pageY || e.clientY;
+//
+//   const rb = range.getBoundingClientRect();
+//
+//   startDraggablePos.x = range.offsetLeft - rb.width / 2;
+//   startDraggablePos.y = range.offsetTop - rb.height / 2;
+//
+//   isDragging = true;
+// });
+//
+// document.addEventListener("mousemove", (e) => {
+//   if (isDragging) {
+//     const x = e.pageX || e.clientX;
+//     const y = e.pageY || e.clientY;
+//
+//     const dx = x - startMousePos.x;
+//     const dy = y - startMousePos.y;
+//
+//     circle.style.left = `${startDraggablePos.x + dx}px`;
+//     circle.style.top = `${startDraggablePos.y + dy}px`;
+//   }
+// });
+//
+// document.addEventListener("mouseup", () => {
+//   isDragging = false;
+// });
 
-  const pb = pointer.getBoundingClientRect();
+let alpha1, alpha2;
 
-  // console.log(pb);
+circle.addEventListener("mousedown", (e) => {
+  const x = e.pageX || e.clientX;
+  const y = e.pageY || e.clientY;
 
-  startDraggablePos.x = pb.left;
-  startDraggablePos.y = pb.top;
+  const rb = range.getBoundingClientRect();
+
+  const dx = x - (range.offsetLeft + rb.width / 2);
+  const dy = rb.height / 2 + range.offsetTop - y;
+
+  alpha1 = (Math.atan2(dy, dx) * 180) / Math.PI;
 
   isDragging = true;
-
-  // console.log(startMousePos.x, startMousePos.y);
-  // console.log(startDraggablePos.x, startDraggablePos.y);
 });
 
-svg.addEventListener("mousemove", (e) => {
+range.addEventListener("mousemove", (e) => {
   if (isDragging) {
     const x = e.pageX || e.clientX;
     const y = e.pageY || e.clientY;
 
-    const dx = x - startMousePos.x;
-    const dy = y - startMousePos.y;
+    const rb = range.getBoundingClientRect();
 
-    pointer.style.cx = `${startDraggablePos.x + dx}px`;
-    pointer.style.cy = `${startDraggablePos.y + dy}px`;
+    const dx = x - (range.offsetLeft + rb.width / 2);
+    const dy = rb.height / 2 + range.offsetTop - y;
+
+    alpha2 = (Math.atan2(dy, dx) * 180) / Math.PI;
+
+    const rotateAngle = `${alpha1 - alpha2}deg`;
+
+    range.style.transform = `rotate(${rotateAngle})`;
   }
 });
 
-svg.addEventListener("mouseup", () => {
+range.addEventListener("mouseup", () => {
   isDragging = false;
 });
