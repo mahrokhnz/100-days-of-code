e93cb79326323152a99c010a199255d435ed2fc5
mahrokh <mahrokh.nz@gmail.com>
2022-02-16 21:52:51 +0330

    feat: add nav and animation

--- projects/fullscreen_menu/script.js
+++ projects/fullscreen_menu/script.js
@@ -1,10 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
 const span = document.getElementsByTagName("span");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const button = document.querySelector(".button");
+const logo = document.querySelector(".logo");
 
 function clickHandler() {
   for (i = 0; i < span.length; i++) {
     span[i].classList.toggle("open");
   }
+  nav.classList.toggle("open");
+  header.classList.toggle("open");
+  button.classList.toggle("open");
+  logo.classList.toggle("open");
 }
 
 hamburger.addEventListener("click", clickHandler);
