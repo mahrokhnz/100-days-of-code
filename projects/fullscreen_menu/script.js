bcf2c5c6e6d399c1d1ff2918c2b41551c14e560b
mahrokh <mahrokh.nz@gmail.com>
2022-02-17 22:34:30 +0330

    refactor: add header hamburger and open menu

--- projects/fullscreen_menu/script.js
+++ projects/fullscreen_menu/script.js
@@ -1,18 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
-const span = document.getElementsByTagName("span");
-const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
-const button = document.querySelector(".button");
-const logo = document.querySelector(".logo");
 
-function clickHandler() {
-  for (i = 0; i < span.length; i++) {
-    span[i].classList.toggle("open");
-  }
-  nav.classList.toggle("open");
+function openHandler() {
   header.classList.toggle("open");
-  button.classList.toggle("open");
-  logo.classList.toggle("open");
 }
 
-hamburger.addEventListener("click", clickHandler);
+hamburger.addEventListener("click", openHandler);
