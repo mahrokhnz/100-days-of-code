3db5801bbe17734db6755e97834666d44323bd61
mahrokh <mahrokh.nz@gmail.com>
2022-02-18 17:37:30 +0330

    refactor: change nav functionality

--- projects/fullscreen_menu/script.js
+++ projects/fullscreen_menu/script.js
@@ -1,8 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
 
 function openHandler() {
   header.classList.toggle("open");
+  nav.classList.toggle("open");
 }
 
 hamburger.addEventListener("click", openHandler);
