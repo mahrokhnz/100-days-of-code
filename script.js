376b8578404b41748291cd54b2b1bf4639acb715
mahrokh <mahrokh.nz@gmail.com>
2022-02-23 22:16:30 +0330

    refactor: will change coding!

--- script.js
+++
@@ -1,6 +0,0 @@
-const flippingChecked = document.querySelector(".flippingChecked");
-
-flippingChecked.addEventListener("click", () => {
-  flippingChecked.classList.toggle("active");
-  console.log("kjhk");
-});
