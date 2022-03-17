c3d5567cf64224f0ccb4ca69b4645d0f037b4fbf
mahrokh <mahrokh.nz@gmail.com>
2022-03-17 20:23:41 +0330

    feat: try to replace new function

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -4,50 +4,21 @@ const backSpace = document.querySelector(".clear");
 
 let beforeNumber, NextNumber;
 
-// const calculatorScreenViewHandler = (type) => {
-//   if (type.innerHTML !== ".") {
-//     if (type.innerHTML !== "=") {
-//       if (calculatorScreen.innerHTML.length > 0) {
-//         calculatorScreen.innerHTML += type.innerHTML;
-//       } else if (type.innerHTML !== "×" && type.innerHTML !== "÷") {
-//         calculatorScreen.innerHTML += type.innerHTML;
-//       }
-//     }
-//   } else {
-//     if (!calculatorScreen.innerHTML.includes(".")) {
-//       calculatorScreen.innerHTML += type.innerHTML;
-//     }
-//   }
-// };
+types.forEach((type) => {
+  type.addEventListener("click", (e) => {
+    if (e.target.matches("button")) {
+      const key = e.target;
+      const action = key.dataset.action;
+      const keyContent = key.textContent;
 
-// // const operators = () => {
-// //   if (calculatorScreen.innerHTML.includes("+")) {
-// //     const slug = calculatorScreen.innerHTML.substring(calculatorScreen.innerHTML.indexOf("+"));
-// //     console.log(slug);
-// //   }
-// // };
-//
-// types.forEach((type) => {
-//   type.addEventListener("click", (e) => {
-//     if (e.target.matches("button")) {
-//       const key = e.target;
-//       const action = key.dataset.action;
-//       const keyContent = key.textContent;
-//
-//       if (display.textContent !== "0" && action !== "calculate") {
-//         display.textContent += keyContent;
-//       }
-//       if (
-//         display.textContent === "0" &&
-//         action === "sum" &&
-//         action === "division"
-//       ) {
-//         display.textContent += keyContent;
-//       }
-//     }
-//   });
-// });
+      if (!action || action === "submission") {
+        display.textContent += keyContent;
+      } else if (display.textContent.length > 0 && action !== "calculate") {
+        display.textContent += keyContent;
+      }
 
-// backSpace.addEventListener("click", () => {
-//   calculatorScreen.innerHTML = calculatorScreen.innerHTML.substring(0, calculatorScreen.innerHTML.length - 1);
-// });
+      if (action === "calculate") {
+      }
+    }
+  });
+});
