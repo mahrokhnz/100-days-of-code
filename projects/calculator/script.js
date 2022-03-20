8b6133ba20af0d573b22add560b66db56e927ef3
mahrokh <mahrokh.nz@gmail.com>
2022-03-20 14:14:47 +0330

    feat: try to replace new function & define new let

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,24 +1,27 @@
-const types = document.querySelectorAll(".type");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".calculatorScreen");
-const backSpace = document.querySelector(".clear");
 
-let beforeNumber, NextNumber;
+let firstNumber = "";
+let secondNumber = "";
+let resultNumber = 0;
+let main = "";
 
-types.forEach((type) => {
-  type.addEventListener("click", (e) => {
-    if (e.target.matches("button")) {
-      const key = e.target;
-      const action = key.dataset.action;
-      const keyContent = key.textContent;
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    main += number.textContent;
+    display.textContent = main;
+    firstNumber = main;
 
-      if (!action || action === "submission") {
-        display.textContent += keyContent;
-      } else if (display.textContent.length > 0 && action !== "calculate") {
-        display.textContent += keyContent;
-      }
-
-      if (action === "calculate") {
-      }
+    if (operators) {
+      secondNumber = main;
     }
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => {
+    main = "";
+    console.log(firstNumber, secondNumber);
   });
 });
