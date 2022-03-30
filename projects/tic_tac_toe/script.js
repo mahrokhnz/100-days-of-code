23206a003f63e572efa66be64b1608943db300b7
mahrokh <mahrokh.nz@gmail.com>
2022-03-30 16:51:57 +0430

    fix: winner function and add isActive boolean

--- projects/tic_tac_toe/script.js
+++ projects/tic_tac_toe/script.js
@@ -5,18 +5,19 @@ const text = document.querySelector(".text");
 
 let playerMarker;
 let gameMarker;
+let isActive = false;
 
 const userFillingCells = () => {
   cells.forEach((cell) => {
+    text.textContent = "Your Turn!";
     cell.addEventListener("click", () => {
-      if (cell.childNodes.length === 0) {
+      if (isActive && cell.childNodes.length === 0) {
         cell.textContent = playerMarker;
         gameFillingCells(gameMarker);
+        winnerHandler();
       }
     });
   });
-
-  winnerHandler();
 };
 
 const gameFillingCells = () => {
@@ -31,48 +32,49 @@ const gameFillingCells = () => {
 
   //MAKE IT SIMPLE
 
-  if (
-    cells[0].textContent.includes(gameMarker) &&
-    cells[2].textContent.includes(gameMarker) &&
-    cells[1].childNodes.length === 0
-  ) {
-    cells[1].textContent = gameMarker;
-  } else if (
-    cells[6].textContent.includes(gameMarker) &&
-    cells[8].textContent.includes(gameMarker) &&
-    cells[7].childNodes.length === 0
-  ) {
-    cells[7].textContent = gameMarker;
-  } else if (
-    cells[0].textContent.includes(gameMarker) &&
-    cells[6].textContent.includes(gameMarker) &&
-    cells[3].childNodes.length === 0
-  ) {
-    cells[3].textContent = gameMarker;
-  } else if (
-    cells[2].textContent.includes(gameMarker) &&
-    cells[8].textContent.includes(gameMarker) &&
-    cells[5].childNodes.length === 0
-  ) {
-    cells[5].textContent = gameMarker;
-  } else if (
-    cells[0].textContent.includes(gameMarker) &&
-    cells[8].textContent.includes(gameMarker) &&
-    cells[4].childNodes.length === 0
-  ) {
-    cells[4].textContent = gameMarker;
-  } else if (
-    cells[2].textContent.includes(gameMarker) &&
-    cells[6].textContent.includes(gameMarker) &&
-    cells[4].childNodes.length === 0
-  ) {
-    cells[4].textContent = gameMarker;
-  } else {
-    const random = Math.floor(Math.random() * corners.length);
-    corners[random].textContent = gameMarker;
-  }
-
   winnerHandler();
+  if (isActive) {
+    if (
+      cells[0].textContent.includes(gameMarker) &&
+      cells[2].textContent.includes(gameMarker) &&
+      cells[1].childNodes.length === 0
+    ) {
+      cells[1].textContent = gameMarker;
+    } else if (
+      cells[6].textContent.includes(gameMarker) &&
+      cells[8].textContent.includes(gameMarker) &&
+      cells[7].childNodes.length === 0
+    ) {
+      cells[7].textContent = gameMarker;
+    } else if (
+      cells[0].textContent.includes(gameMarker) &&
+      cells[6].textContent.includes(gameMarker) &&
+      cells[3].childNodes.length === 0
+    ) {
+      cells[3].textContent = gameMarker;
+    } else if (
+      cells[2].textContent.includes(gameMarker) &&
+      cells[8].textContent.includes(gameMarker) &&
+      cells[5].childNodes.length === 0
+    ) {
+      cells[5].textContent = gameMarker;
+    } else if (
+      cells[0].textContent.includes(gameMarker) &&
+      cells[8].textContent.includes(gameMarker) &&
+      cells[4].childNodes.length === 0
+    ) {
+      cells[4].textContent = gameMarker;
+    } else if (
+      cells[2].textContent.includes(gameMarker) &&
+      cells[6].textContent.includes(gameMarker) &&
+      cells[4].childNodes.length === 0
+    ) {
+      cells[4].textContent = gameMarker;
+    } else {
+      const random = Math.floor(Math.random() * corners.length);
+      corners[random].textContent = gameMarker;
+    }
+  }
 };
 
 const winnerHandler = () => {
@@ -87,7 +89,7 @@ const winnerHandler = () => {
     [cells[2], cells[4], cells[6]],
   ];
 
-  //MAKE IT SIMPLE && FIX && DEFINE BOOLEAN ISACTIVE
+  //MAKE IT SIMPLE
 
   for (let i = 0; i < winnerArrays.length; i++) {
     if (
@@ -95,13 +97,15 @@ const winnerHandler = () => {
         element.textContent.includes(gameMarker)
       )
     ) {
-      text.textContent = "game";
+      isActive = false;
+      text.textContent = "Game Over!";
     } else if (
       winnerArrays[i].every((element) =>
         element.textContent.includes(playerMarker)
       )
     ) {
-      text.textContent = "player";
+      isActive = false;
+      text.textContent = "You Won!";
     }
   }
 };
@@ -113,6 +117,8 @@ markers.forEach((marker) => {
 
     playerMarker = marker.textContent;
     gameMarker = playerMarker === "X" ? "O" : "X";
+
+    isActive = true;
 
     userFillingCells();
     gameFillingCells();
