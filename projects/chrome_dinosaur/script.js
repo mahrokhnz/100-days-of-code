12d178e0052de0ed8d37bd3b95237f9aaab35200
mahrokh <mahrokh.nz@gmail.com>
2022-04-01 12:31:54 +0430

    feat: add jumping event listener

---
+++ projects/chrome_dinosaur/script.js
@@ -0,0 +1,13 @@
+const dinosaur = document.querySelector(".dinosaur");
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    dinosaur.classList.add("active");
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (e.code === "Space") {
+    dinosaur.classList.remove("active");
+  }
+});
