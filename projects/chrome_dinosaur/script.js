92fe119c6f567ed673c8e50a4e19de74205b64b1
mahrokh <mahrokh.nz@gmail.com>
2022-04-09 22:01:30 +0430

    feat: add animation frame

--- projects/chrome_dinosaur/script.js
+++ projects/chrome_dinosaur/script.js
@@ -1,17 +1,43 @@
 const dinosaur = document.querySelector(".dinosaur");
 const ground = document.querySelector(".ground");
+const cactus = document.querySelector(".cactus");
 
 let frameId;
+let dinosaurY;
+let cactusX;
+let reqAnimation;
+let playing = true;
 
-function jumping() {
-  dinosaur.classList.add("active");
-  ground.classList.add("active");
+function conflict() {
+  let dinosaurY = dinosaur.getBoundingClientRect().top;
+  let cactusX = cactus.getBoundingClientRect().left;
+
+  let reqAnimation = window.requestAnimationFrame(conflict);
+
+  window.cancelAnimationFrame(reqAnimation);
 }
 
-document.addEventListener("keydown", jumping);
+function jumping() {
+  playing && dinosaur.classList.add("active");
+}
+
 dinosaur.addEventListener("animationend", () => {
   dinosaur.classList.remove("active");
 });
+
+function moving() {
+  if (playing) {
+    ground.classList.add("active");
+    window.cancelAnimationFrame(reqAnimation);
+    conflict();
+  }
+}
+
 ground.addEventListener("animationend", () => {
   ground.classList.remove("active");
+});
+
+document.addEventListener("keydown", () => {
+  jumping();
+  moving();
 });
