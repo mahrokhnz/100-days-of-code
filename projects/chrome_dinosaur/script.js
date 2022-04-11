78f86ca2725a433d9f10b100d4702db5d0f3c886
mahrokh <mahrokh.nz@gmail.com>
2022-04-11 21:56:40 +0430

    feat: add restart button and stop animation

--- projects/chrome_dinosaur/script.js
+++ projects/chrome_dinosaur/script.js
@@ -3,14 +3,21 @@ const ground = document.querySelector(".ground");
 const cactus = document.querySelector(".cactus");
 const restart = document.querySelector(".restart");
 const main = document.querySelector(".main");
+const gameOver = document.querySelector(".gameOver");
+const button = document.querySelector(".button");
 
 let reqAnimation;
-let isPlaying = true;
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "Space" && isPlaying) {
+  if (e.code === "Space") {
     dinosaur.classList.add("active");
     ground.classList.add("active");
+    // ground.style.animation = "none";
+    // ground.style.offsetWidth;
+    // ground.style.animation = "moving 4s linear";
+    // dinosaur.style.animation = "none";
+    // dinosaur.style.offsetWidth;
+    // dinosaur.style.animation = "jumping 0.8s linear";
   }
 });
 
@@ -18,26 +25,35 @@ dinosaur.addEventListener("animationend", () => {
   dinosaur.classList.remove("active");
 });
 
-restart.addEventListener("click", () => {
-  isPlaying = true;
+ground.addEventListener("animationend", () => {
+  ground.classList.remove("active");
+});
+
+button.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    // e.stopPropagation();
+    // console.log("jj");
+    //ADD RESTART BUTTON
+  }
 });
 
 function overlap() {
   let dinosaurY =
     dinosaur.getBoundingClientRect().top - main.getBoundingClientRect().top;
-  let cactusX = cactus.getBoundingClientRect().left;
+  let cactusX =
+    cactus.getBoundingClientRect().left - main.getBoundingClientRect().left;
+
+  console.log(dinosaurY, cactusX);
 
   reqAnimation = window.requestAnimationFrame(overlap);
 
   //IN DIFFERENT WINDOW SIZES SHOULD BE CHECKED
 
-  if (dinosaurY <= 172 && dinosaurY > 122 && cactusX <= 90 && cactusX >= 0) {
+  if (dinosaurY <= 172 && dinosaurY > 122 && cactusX <= 50 && cactusX >= 0) {
     window.cancelAnimationFrame(reqAnimation);
-    isPlaying = false;
-
-    //DOESNT WORK PROPERLY
-
     ground.style.animationPlayState = "paused";
+    dinosaur.style.animationPlayState = "paused";
+    gameOver.style.display = "block";
   }
 }
 
