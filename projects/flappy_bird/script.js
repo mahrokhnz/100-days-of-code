48b672316378f814efb74c916e17e552c6b70a78
mahrokh <mahrokh.nz@gmail.com>
2022-04-30 21:10:03 +0430

    refactor: change eventHandler

--- projects/flappy_bird/script.js
+++ projects/flappy_bird/script.js
@@ -1,19 +1,20 @@
 const flappyBird = document.querySelector(".flappyBird");
 
 let randomTop = Math.random() * 100;
-document.addEventListener("keypress", (e) => {
-  if (e.code === "Space") {
-    flappyBird.classList.add("active");
-  }
-});
 
 document.addEventListener("keyup", (e) => {
   flappyBird.classList.remove("active");
   console.log("jj");
 });
 
-// const animationHandler = () => {
-//   requestAnimationFrame(animationHandler);
-// };
-//
-// requestAnimationFrame(animationHandler);
+const animationHandler = () => {
+  document.addEventListener("keypress", (e) => {
+    if (e.code === "Space") {
+      flappyBird.style.top += "10px";
+    }
+  });
+
+  requestAnimationFrame(animationHandler);
+};
+
+requestAnimationFrame(animationHandler);
