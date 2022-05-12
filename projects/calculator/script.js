7d2deb5223a9f62b529ba271bf738ae75e3faffa
mahrokh <mahrokh.nz@gmail.com>
2022-05-12 11:05:56 +0430

    feat: add operators basic functions and add display value

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,10 +1,40 @@
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
 
 const handleInsertNumber = (e) => {
-  console.log(display.innerHTML);
+  display.value += e.target.innerText;
+};
+
+const handleInsertOperator = (e) => {
+  console.log(display.value.includes(operators));
+  //Clear
+  if (display.value.length > 0 && e.target.innerText === "c") {
+    display.value = display.value.slice(0, -1);
+  }
+
+  //÷ and ×
+  if (
+    display.value.length > 0 &&
+    (e.target.innerText === "÷" || e.target.innerText === "×")
+  ) {
+    display.value += e.target.innerText;
+  }
+
+  if (
+    e.target.innerText !== "c" &&
+    e.target.innerText !== "÷" &&
+    e.target.innerText !== "×" &&
+    e.target.innerText !== "="
+  ) {
+    display.value += e.target.innerText;
+  }
 };
 
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => handleInsertNumber(e));
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (e) => handleInsertOperator(e));
 });
