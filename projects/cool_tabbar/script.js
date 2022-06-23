b7eec79fcb61c7fb36dccc96560db7c26cd28f95
mahrokh <mahrokh.nz@gmail.com>
2022-06-23 11:34:29 +0430

    fix: indicator moving

--- projects/cool_tabbar/script.js
+++ projects/cool_tabbar/script.js
@@ -1,18 +1,18 @@
 const icons = document.querySelectorAll(".icon");
+const solidIcons = document.querySelectorAll(".solid");
+const tabbar = document.querySelector(".tabbar");
 const indicator = document.querySelector(".indicator");
 
 icons.forEach((icon) => {
   icon.addEventListener("click", () => {
-    const iconPos =
-      icon.getBoundingClientRect().left +
-      icon.getBoundingClientRect().width / 2;
-
+    const iconPos = Math.abs(
+      tabbar.getBoundingClientRect().left -
+        (icon.getBoundingClientRect().left +
+          icon.getBoundingClientRect().width / 2)
+    );
     indicator.style.left = iconPos + "px";
 
-    if (icon.classList.contains(".solid")) {
-      icon.classList.add("active");
-    } else {
-      console.log(icon.classList);
-    }
+    icon.parentElement.classList.add("active");
   });
+  icon.parentElement.classList.remove("active");
 });
