d6cdc5a35f8b2db4a520f3c1ec1aa87058c4f6ad
mahrokh <mahrokh.nz@gmail.com>
2022-06-24 12:47:10 +0430

    fix: icons clip

--- projects/cool_tabbar/script.js
+++ projects/cool_tabbar/script.js
@@ -2,9 +2,14 @@ const icons = document.querySelectorAll(".icon");
 const solidIcons = document.querySelectorAll(".solid");
 const tabbar = document.querySelector(".tabbar");
 const indicator = document.querySelector(".indicator");
+const menuItems = document.querySelectorAll(".menuItem");
 
 icons.forEach((icon) => {
   icon.addEventListener("click", () => {
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.remove("active");
+    });
+
     const iconPos = Math.abs(
       tabbar.getBoundingClientRect().left -
         (icon.getBoundingClientRect().left +
@@ -12,7 +17,10 @@ icons.forEach((icon) => {
     );
     indicator.style.left = iconPos + "px";
 
-    icon.parentElement.classList.add("active");
+    if (!icon.parentElement.classList.contains("active")) {
+      icon.parentElement.classList.add("active");
+    }
   });
-  icon.parentElement.classList.remove("active");
 });
+
+// TODO: ADD BUBBLE BLOB AND FIX TIMING
