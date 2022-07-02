75e438753e89cbd4984dac9c21e689fc46130c84
mahrokh <mahrokh.nz@gmail.com>
2022-07-02 22:07:58 +0430

    feat: create new function for moving rect (not working!)

--- projects/red_rect/script.js
+++ projects/red_rect/script.js
@@ -13,28 +13,36 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 document.addEventListener("keydown", (e) => movingRedRect(e));
 
+const drawRect = (newY) => {
+  ctx.clearRect(20, y, 50, 50);
+
+  ctx.beginPath();
+  ctx.rect(20, newY, 50, 50);
+  ctx.fillStyle = "red";
+  ctx.fillRect(20, newY, 50, 50);
+};
+
 const movingRedRect = (e) => {
   if (e.key === "ArrowDown") {
     setInterval(() => {
-      y += 3;
+      let newY = y + 2;
+      drawRect(newY);
     }, 100);
   }
 
   if (e.key === "ArrowUp") {
     setInterval(() => {
-      y -= 3;
+      let newY = y - 2;
+      drawRect(newY);
+
+      drawRect(y);
     }, 100);
   }
-  ctx.beginPath();
-  ctx.rect(20, y, 50, 50);
-  ctx.fillStyle = "red";
-  ctx.fillRect(20, y, 50, 50);
 
   window.requestAnimationFrame(movingRedRect);
 };
 window.requestAnimationFrame(movingRedRect);
 
-//RED RECT
 ctx.beginPath();
 ctx.rect(20, y, 50, 50);
 ctx.fillStyle = "red";
