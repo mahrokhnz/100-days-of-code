4c53ecbf8fa59b1b30801296ce5100a6c10cdd51
mahrokh <mahrokh.nz@gmail.com>
2022-07-04 21:34:35 +0430

    refactor: add classes

--- projects/red_rect/script.js
+++ projects/red_rect/script.js
@@ -1,49 +1,15 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+import Rect from "./classes/rect";
 
-let y = 100;
+const canvas = document.getElementById("myCanvas");
 
 //FULL SCREEN CANVAS
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const ctx = canvas.getContext("2d");
+
 //COLORIZE
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-document.addEventListener("keydown", (e) => movingRedRect(e));
-
-const drawRect = (newY) => {
-  ctx.clearRect(20, y, 50, 50);
-
-  ctx.beginPath();
-  ctx.rect(20, newY, 50, 50);
-  ctx.fillStyle = "red";
-  ctx.fillRect(20, newY, 50, 50);
-};
-
-const movingRedRect = (e) => {
-  if (e.key === "ArrowDown") {
-    setInterval(() => {
-      let newY = y + 2;
-      drawRect(newY);
-    }, 100);
-  }
-
-  if (e.key === "ArrowUp") {
-    setInterval(() => {
-      let newY = y - 2;
-      drawRect(newY);
-
-      drawRect(y);
-    }, 100);
-  }
-
-  window.requestAnimationFrame(movingRedRect);
-};
-window.requestAnimationFrame(movingRedRect);
-
-ctx.beginPath();
-ctx.rect(20, y, 50, 50);
-ctx.fillStyle = "red";
-ctx.fillRect(20, y, 50, 50);
+new Rect(100, 100, 400, 400).setFill("blue").draw(ctx);
