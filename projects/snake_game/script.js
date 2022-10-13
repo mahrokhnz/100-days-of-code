a46dafd6e4b653febff48d5b5ff37629a0ca45a7
mahrokh <mahrokh.nz@gmail.com>
2022-10-13 19:02:44 +0330

    feat: add text of step 1

---
+++ projects/snake_game/script.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let isPlaying = true;
+let rectReq;
+
+const width = canvas.width
+const height = canvas.height
+
+ctx.font = "400px Arial";
+ctx.fillText("Snake",width/9 , height/2)
+
+ctx.font = "100px Arial";
+ctx.fillText("Easy",width/7 , 3 * height/4)
+ctx.fillText("Medium",4 * width/11 , 3 * height/4)
+ctx.fillText("Hard",4 * width/6 , 3 * height/4)
+
+ctx.beginPath()
+ctx.rect(80, 2 * height/6, 50, 50)
+ctx.fillRect(80, 2 * height/6, 50, 50)
+
+if (isPlaying) {
+    cancelAnimationFrame(rectReq)
+}
+
+const rectAnimate = () => {
+    if (!isPlaying) {
+        ctx.rect(width - 120, 2 * height/6, 50, 50)
+        ctx.fillRect(width - 120, 2 * height/6, 50, 50)
+
+        rectReq = requestAnimationFrame(rectAnimate)
+    }
+}
+
+rectReq = requestAnimationFrame(rectAnimate)
+
