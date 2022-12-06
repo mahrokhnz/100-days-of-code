65859c4c71bea288582d69cec38110cadb124049
mahrokh <mahrokh.nz@gmail.com>
2022-12-06 19:46:34 +0330

    fix: snake move

--- projects/snake_game/script.js
+++ projects/snake_game/script.js
@@ -2,70 +2,79 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 let isPlaying = true;
-let level = 'easy'
+let level = "easy";
 
 let x = 0;
 let y = 0;
 
-const array = []
-const snakeArray = []
+const array = [];
+const snakeArray = [];
 
-let i = 0
+let i = 0;
 while (i <= 673) {
-    array.push({x: x, y: y})
+  array.push({ x: x, y: y });
 
-    ctx.strokeStyle = "gray";
-    ctx.strokeRect(x, y, 20, 20);
+  ctx.strokeStyle = "gray";
+  ctx.strokeRect(x, y, 20, 20);
 
-    x += 20
+  x += 20;
 
-    if (x > 520) {
-        y += 20
-        x = 0
-    }
+  if (x > 520) {
+    y += 20;
+    x = 0;
+  }
 
-    i ++
+  i++;
 }
 
-const keyHandler = (snake, type) => {
-    console.log(snake, type)
-}
+// const keyHandler = (snake, type) => {
+//   console.log(snake, type);
+// };
+//
+// document.addEventListener("keydown", (e) => {
+//   if (e.code === "ArrowUp") {
+//     keyHandler(snakeArray, "ArrowUp");
+//   } else if (e.code === "ArrowDown") {
+//     keyHandler(snakeArray, "ArrowDown");
+//   } else if (e.code === "ArrowRight") {
+//     keyHandler(snakeArray, "ArrowRight");
+//   } else if (e.code === "ArrowLeft") {
+//     keyHandler(snakeArray, "ArrowLeft");
+//   }
+// });
 
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'ArrowUp') {
-        keyHandler(snakeArray, 'ArrowUp')
-    } else if (e.code === 'ArrowDown') {
-        keyHandler(snakeArray, 'ArrowDown')
-    } else if (e.code === 'ArrowRight') {
-        keyHandler(snakeArray, 'ArrowRight')
-    } else if (e.code === 'ArrowLeft') {
-        keyHandler(snakeArray, 'ArrowLeft')
-    }
-})
-
-const snakeColor = (snakeBody) => {
+const snakeColor = (snakeBody, type) => {
+  if (type === "color") {
     ctx.fillStyle = "red";
     ctx.fillRect(snakeBody.x, snakeBody.y, 20, 20);
-}
+  }
+
+  if (type === "clear") {
+    ctx.clearRect(snakeBody.x, snakeBody.y, 20, 20);
+
+    ctx.strokeStyle = "gray";
+    ctx.strokeRect(snakeBody.x, snakeBody.y, 20, 20);
+  }
+};
 
 const snakeMove = () => {
-    let i = 0
+  let i = 0;
 
-    setInterval(() => {
-        if (snakeArray.length <= 3) {
-            snakeArray.push(array[i])
-            snakeColor(array[i])
-        } else {
-            snakeArray.shift()
-            snakeArray.push(array[i])
-            snakeColor(array[i])
-        }
+  setInterval(() => {
+    if (snakeArray.length <= 3) {
+      snakeArray.push(array[i]);
+      snakeColor(array[i], "color");
+    } else {
+      const extraRect = snakeArray.shift();
+      snakeColor(extraRect, "clear");
+      snakeArray.push(array[i]);
+      snakeColor(array[i], "color");
+    }
 
-        i++
-    }, 1000)
-}
+    i++;
+  }, 1000);
+};
 
-snakeMove()
+snakeMove();
 
-
-// TODO: snakeArray should delete first element
+// TODO: HANDLE SNAKE MOVEMENT
