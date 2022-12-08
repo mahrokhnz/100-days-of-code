6f3ccdf5c241f2cae81892bcfccc1d92ce6955fd
mahrokh <mahrokh.nz@gmail.com>
2022-12-08 14:21:08 +0330

    refactor: moving

--- projects/snake_game/script.js
+++ projects/snake_game/script.js
@@ -29,59 +29,22 @@ while (i <= 674) {
 
 const keyHandler = (type) => {
   const snakeHead = snakeCellsArray[snakeCellsArray.length - 1];
-  const newSnakeCellsArray = [];
-
-  if (type === "ArrowUp") {
-    // FAIL
-    if (snakeHead.y === 0) {
-      alert("Failed");
-      // TODO: RESTART THE GAME AFTER SHOWING BUTTON TO TRUE THE IS PLAYING
-    }
-  }
 
   if (type === "ArrowDown") {
-    // FAIL
-    if (snakeHead.y === 480) {
-      alert("Failed");
-      // TODO: RESTART THE GAME AFTER SHOWING BUTTON TO TRUE THE IS PLAYING
-    }
-
-    snakeCellsArray.forEach((cell) => {
-      cell.y += 20;
-      newSnakeCellsArray.push(cell);
+    snakeCellsArray.forEach((snakeCell) => {
+      snakeCell.y += 20;
     });
 
-    snakeMove(newSnakeCellsArray);
+    // TODO: SHOULD FIND PROPER ARRAY FOR SNAKE (FIRST MOVE HEAD)
   }
 
-  if (type === "ArrowRight") {
-    // FAIL
-    if (snakeHead.x === 480) {
-      alert("Failed");
-      // TODO: RESTART THE GAME AFTER SHOWING BUTTON TO TRUE THE IS PLAYING
-    }
-  }
-
-  if (type === "ArrowLeft") {
-    // FAIL
-    if (snakeHead.x === 0) {
-      alert("Failed");
-      // TODO: RESTART THE GAME AFTER SHOWING BUTTON TO TRUE THE IS PLAYING
-    }
-  }
+  snakeMove(snakeCellsArray);
 };
 
 document.addEventListener("keydown", (e) => {
-  if (e.code === "ArrowUp") {
-    keyHandler("ArrowUp");
-  } else if (e.code === "ArrowDown") {
+  if (e.code === "ArrowDown") {
     keyHandler("ArrowDown");
   }
-  // else if (e.code === "ArrowRight") {
-  //   keyHandler(snakeCellsArray, "ArrowRight");
-  // } else if (e.code === "ArrowLeft") {
-  //   keyHandler(snakeCellsArray, "ArrowLeft");
-  // }
 });
 
 const snakeColor = (snakeBody, type) => {
@@ -100,17 +63,21 @@ const snakeColor = (snakeBody, type) => {
   }
 };
 
-const snakeMove = (cellsArray) => {
+const snakeCellsBuilder = (snakeArray) => {
+  snakeMove(snakeArray);
+};
+
+const snakeMove = (snakeArray) => {
   let i = 0;
 
   setInterval(() => {
-    if (cellsArray.length <= 3) {
-      snakeCellsArray.push(array[i]);
+    if (snakeArray.length <= 3) {
+      snakeArray.push(array[i]);
       snakeColor(array[i], "color");
     } else {
-      const extraRect = snakeCellsArray.shift();
+      const extraRect = snakeArray.shift();
       snakeColor(extraRect, "clear");
-      snakeCellsArray.push(array[i]);
+      snakeArray.push(array[i]);
       snakeColor(array[i], "color");
     }
 
@@ -119,7 +86,7 @@ const snakeMove = (cellsArray) => {
 };
 
 if (isPlaying) {
-  snakeMove([array[0]]);
+  snakeMove(snakeCellsArray);
 }
 
 // TODO: HANDLE SNAKE MOVEMENT
