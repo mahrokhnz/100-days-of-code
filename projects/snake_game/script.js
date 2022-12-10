1db792d5bbec29ccf274850a82859852f4836917
mahrokh <mahrokh.nz@gmail.com>
2022-12-10 21:22:30 +0330

    feat: add new functions for building, colorize and direction of moving snake

--- projects/snake_game/script.js
+++ projects/snake_game/script.js
@@ -8,6 +8,7 @@ const ctx = canvas.getContext("2d");
 const snake = {
   direction: "right",
   cellPositions: [],
+  head: { x: 60, y: 0 },
   bodyLength: 4,
   canTurn: true,
   interval: 1000,
@@ -36,5 +37,50 @@ while (i <= 674) {
   i++;
 }
 
+const snakeColorize = (snakeCell, cell) => {
+  if (snakeCell) {
+    if (cell === "alive") {
+      ctx.fillStyle = "red";
+      ctx.fillRect(snakeCell.x, snakeCell.y, 20, 20);
+    }
+
+    if (cell === "dead") {
+      ctx.clearRect(snakeCell.x, snakeCell.y, 20, 20);
+
+      ctx.strokeStyle = "gray";
+      ctx.strokeRect(snakeCell.x, snakeCell.y, 20, 20);
+    }
+  }
+};
+
+const snakeMover = (snakeCells) => {
+  if (snake.direction === "right") {
+    // Delete Tail
+    const snakeTail = snakeCells.shift();
+    snakeColorize(snakeTail, "dead");
+    // Define new Head
+    snake.head = { x: snake.head.x + gridSize, y: snake.head.y };
+    snakeCells.push(snake.head);
+    snakeColorize(snake.head, "alive");
+  }
+};
+
+const snakeBuilder = () => {
+  const snakeCells = [array[0], array[1], array[2], array[3]];
+  // Colorize
+  snakeCells.forEach((snakeCell) => {
+    snakeColorize(snakeCell, "alive");
+  });
+  // Give to snake object
+  snake.cellPositions = snakeCells;
+  // Move snake
+  snakeMover(snakeCells);
+};
+
+snakeBuilder();
+
 // TODO
 // MOVE SNAKE BY KEYS (SWITCH AND CASE)
+// DEFINE MOVEMENT BY DIRECTION
+// IN ARRAY OF SNAKE ELEMENT[0] WILL SHIFT AND HEAD WILL CHANGE ACCORDING TO DIRECTION
+// ADD KEY DOWN AND INTERVAL FOR AUTO MOVING
