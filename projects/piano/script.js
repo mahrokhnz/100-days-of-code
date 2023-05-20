450bcd64fac85bcec9de7160df3fd90f26c88b5e
mahrokh <mahrokh.nz@gmail.com>
2023-05-20 22:26:25 +0430

    feat: add frequency list and basic function

--- projects/piano/script.js
+++ projects/piano/script.js
@@ -1,7 +1,48 @@
 const doFa = document.querySelector(".doFa");
 
-const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const frequencies = {
+  doFa: 130.8128,
+  doFaDiez: 138.5913,
+  reFaBemol: 138.5913,
+  reFa: 146.8324,
+  reFaDiez: 155.5635,
+  miFaBemol: 155.5635,
+  miFa: 164.8138,
+  faFa: 174.6141,
+  faFaDiez: 184.9972,
+  solFaBemol: 184.9972,
+  solFa: 195.9977,
+  solFaDiez: 207.6523,
+  laFaBemol: 207.6523,
+  laFa: 220.0,
+  laFaDiez: 233.0819,
+  siFaBemol: 233.0819,
+  siFa: 246.9417,
+  do: 261.6256,
+  doDiez: 277.1826,
+  reSolBemol: 277.1826,
+  reSol: 293.6648,
+  reSolDiez: 311.127,
+  miSolBemol: 311.127,
+  miSol: 329.6276,
+  faSol: 349.2282,
+  faSolDiez: 369.9944,
+  solSolBemol: 369.9944,
+  solSol: 391.9954,
+  solSolDiez: 415.3047,
+  laSolBemol: 415.3047,
+  laSol: 440.0,
+  laSolDiez: 466.1638,
+  siSolBemol: 466.1638,
+  siSol: 493.8833,
+  doSol: 523.2511,
+  doSolDiez: 554.3653,
+};
 
-doFa.addEventListener("click", () => {
-  audio.play();
-});
+// const context = new AudioContext();
+// const o = context.createOscillator();
+// const g = context.createGain();
+// o.connect(g);
+// g.connect(context.destination);
+// o.frequency.value = 440.0;
+// o.start(0);
